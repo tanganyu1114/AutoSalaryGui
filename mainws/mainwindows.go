da533cf1186888bf33cdb2c1cf1d8450ac4b5e02
tanganyu <569386693@qq.com>
2019-12-31 17:11:03 +0800

    调整布局

--- mainws/mainwindows.go
+++ mainws/mainwindows.go
@@ -14,6 +14,7 @@ type MainGui struct {
 	FilePb    *walk.PushButton
 	ResetPb   *walk.PushButton
 	SendPb    *walk.PushButton
+	LogPb     *walk.PushButton
 	ShowLabel *walk.Label
 	ShowView  *walk.WebView
 }
@@ -29,54 +30,72 @@ func MainShow() {
 		Size:     Size{Width: 640, Height: 400},
 		Layout:   VBox{},
 		Children: []Widget{
-			PushButton{
-				AssignTo: &mg.LoginPb,
-				Text:     "登陆",
-				Visible:  true,
-				OnClicked: func() {
-					//打开登陆配置界面
-					if cmd, err := loginws.LoginWs(mg.Window, &li); err != nil {
-						log.Print(err)
-					} else if cmd == walk.DlgCmdOK {
-						//保存用户信息
-						SaveLogin(&li)
-					}
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					PushButton{
+						AssignTo:   &mg.LoginPb,
+						Text:       "登陆",
+						Visible:    true,
+						ColumnSpan: 2,
+						OnClicked: func() {
+							//打开登陆配置界面
+							if cmd, err := loginws.LoginWs(mg.Window, &li); err != nil {
+								log.Print(err)
+							} else if cmd == walk.DlgCmdOK {
+								//保存用户信息
+								SaveLogin(&li)
+							}
+						},
+					},
+					PushButton{
+						AssignTo: &mg.FilePb,
+						Text:     "请选择工资文件(xlsx)",
+						Visible:  true,
+						OnClicked: func() {
+							//打开选择文件窗口，获取文件路径以及文件名
+						},
+					},
+					PushButton{
+						AssignTo: &mg.ResetPb,
+						Text:     "重置",
+						Visible:  true,
+						OnClicked: func() {
+							//重置选中的文件，清空预览窗口
+						},
+					},
+					PushButton{
+						AssignTo: &mg.SendPb,
+						Text:     "发送邮件",
+						Visible:  true,
+						OnKeyUp: func(key walk.Key) {
+							if key == walk.KeyS {
+								mg.FilePb.SetText("")
+							}
+						},
+					},
+					PushButton{
+						AssignTo: &mg.LogPb,
+						Text:     "查看历史记录",
+						Visible:  true,
+						OnKeyUp: func(key walk.Key) {
+							if key == walk.KeyS {
+								mg.FilePb.SetText("")
+							}
+						},
+					},
+					Label{
+						AssignTo:   &mg.ShowLabel,
+						ColumnSpan: 2,
+						Visible:    true,
+						Text:       "预览邮件信息：",
+					},
+					WebView{
+						AssignTo:   &mg.ShowView,
+						Visible:    true,
+						ColumnSpan: 2,
+					},
 				},
-			},
-			PushButton{
-				AssignTo: &mg.FilePb,
-				Text:     "请选择工资文件(xlsx)",
-				Visible:  true,
-				OnClicked: func() {
-					//打开选择文件窗口，获取文件路径以及文件名
-				},
-			},
-			PushButton{
-				AssignTo: &mg.ResetPb,
-				Text:     "重置",
-				Visible:  true,
-				OnClicked: func() {
-					//重置选中的文件，清空预览窗口
-				},
-			},
-			PushButton{
-				AssignTo: &mg.SendPb,
-				Text:     "发送邮件",
-				Visible:  true,
-				OnKeyUp: func(key walk.Key) {
-					if key == walk.KeyS {
-						mg.FilePb.SetText("")
-					}
-				},
-			},
-			Label{
-				AssignTo: &mg.ShowLabel,
-				Visible:  true,
-				Text:     "预览邮件信息：",
-			},
-			WebView{
-				AssignTo: &mg.ShowView,
-				Visible:  true,
 			},
 		},
 	}
